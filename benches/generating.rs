17a991c5f819c46529a058cdd21b71afe55b6b9b
Peter C. S. Scholtens <peter.scholtens@xs4all.nl>
2017-02-26 22:50:29 +0100

    First testbench created for random filling SudokuField.

--- benches/generating.rs
+++ benches/generating.rs
@@ -1,2 +1,19 @@
+#![feature(test)]
 // Testbench to measure how fast a new Sudoku puzzle can be generated.
 extern crate sudoku;
+extern crate test;
+
+use sudoku::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+
+    #[bench]
+	fn bench_fill_randomly(b: &mut Bencher) {
+		let mut field  = SudokuField::new();
+		b.iter(|| field.fill_randomly() );
+    }
+}
